3fa95376308facf7db233ed9064f6937cacccb3d
smallikarjuna33 <smallikarjuna1996@gmail.com>
2019-11-09 13:27:32 +0530

    First Commit

---
+++ pvco_Db/src/main/java/com/pvco_Db/App.java
@@ -0,0 +1,13 @@
+package com.pvco_Db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
